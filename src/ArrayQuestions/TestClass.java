fcbe538c1df96f721443f965565e52567e0355d9
shashwatdixit31 <shashwatdixit31@gmail.com>
2020-02-03 00:01:33 +0530

    Adding my first test class

---
+++ src/ArrayQuestions/TestClass.java
@@ -0,0 +1,9 @@
+package ArrayQuestions;
+
+public class TestClass {
+    public static void main(String args[]){
+        for(int i=0;i<10;i++) {
+            System.out.println("Hello there");
+        }
+    }
+}
